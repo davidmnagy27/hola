bcd8f4f678169474e18622cf2e175c7ced297dbf
David Nagy <davidmnagy27@gmail.com>
2019-04-10 16:47:57 -0400

    creating views

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+//require the autoload file
+require_once('vendor/autoload.php');
 
-
-ini_set
-('
-display_errors
-', 1);
-error_reporting
-(
-    E_ALL
-);
-//Require the autoload file
-require_once
-('vendor/
-autoload.php
-');
 //Create an instance of the Base class
-$f3 = Base::
-instance
-();
-//Run fat free
-    $f3
+$f3 = Base::instance();
 
->run();
+//debugger
+$f3->set('DEBUG',3);
+
+//Define a default route
+$f3->route('GET /', function()
+{
+   $view = new Template();
+   echo $view->render('Views/home2.html');
+
+
+});
+
+//Run fat free
+$f3->run();
